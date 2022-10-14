2327b4dac444a28b3e380a52d260a5812dbd7936
maniek9101 <janczakpawelek@gmail.com>
2022-10-14 15:04:25 +0200

    add some stuff

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>boiler plate 2.0</h1>
+    <h2>it's small header</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat aut libero dolores laboriosam repellendus autem in nam quibusdam quam eius, alias possimus esse ex illum. Quo aliquam facere obcaecati dolores?</p>
+
+    <?php __DIR__ . '/controll.php'; ?>
+
+</body>
+</html>
